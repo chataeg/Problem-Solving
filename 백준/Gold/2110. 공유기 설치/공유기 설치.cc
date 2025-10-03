2fa6b39f898845e44aa66ade4d55885ad2352364
Tae Gwan Cha <38041722+chataeg@users.noreply.github.com>
2025-10-03 21:50:32 +0900

    [Gold IV] Title: 공유기 설치, Time: 84 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, c;
+	cin >> n >> c;
+
+	vector<int> v;
+
+	while (n--)
+	{
+		int input;
+		cin >> input;
+		v.push_back(input);
+	}
+
+	long long st = 0;
+	long long ed = 1000000000;
+
+	long long ans = 0;
+	
+	sort(v.begin(), v.end());
+
+	while (st <= ed)
+	{
+		long long mid = (st + ed + 1) / 2; 
+
+		long long cnt = 1;
+
+		auto iter = v.begin();
+		
+		while (iter != v.end())
+		{
+			if (lower_bound(iter, v.end(), *iter + mid) != v.end())
+			{
+				iter = lower_bound(iter, v.end(), *iter + mid);
+				cnt++;
+			}
+			else iter++;
+		}
+		
+		if (cnt >= c)
+		{
+			st = mid + 1;
+			ans = mid;
+		}
+		else
+			ed = mid - 1;
+	}
+
+	cout << ans;
+
+	return 0;
+}
