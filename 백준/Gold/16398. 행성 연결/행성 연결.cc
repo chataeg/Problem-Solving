2c4553361ab7d9f6a2f5d9e72b6a3a2e1db1f69d
Tae Gwan Cha <38041722+chataeg@users.noreply.github.com>
2025-10-13 16:39:27 +0900

    [Gold IV] Title: 행성 연결, Time: 176 ms, Memory: 11372 KB -BaekjoonHub

--- 백준/Gold/16398. 행성 연결/행성 연결.cc
+++ 백준/Gold/16398. 행성 연결/행성 연결.cc
@@ -8,14 +8,39 @@
 using namespace std;
 int n, m;
 
-vector<pair<int,int>> adj[1007];
-int check[1007];
+vector<int> p(1007, -1);
+
+
+int find(int num)
+{
+	if (p[num] < 0)
+		return num;
+	return p[num] = find(p[num]); // 재귀로 인해 타고 온 길을 root 로 최적화
+}
+
+bool uni(int u, int v)
+{
+	u = find(u);  // 루트 찾기
+	v = find(v);  // 루트 찾기
+
+	if (u == v)
+		return false; // 루트가 같은 경우
+	if (p[u] < p[v]) // u 의 랭크가 더 큰 경우
+		swap(u, v);  // swap  (트리 높이 최대한 낮게 유지하기 위해)
+	if (p[u] == p[v]) // 랭크가 같은 경우 
+		p[u]--;		// u 의 랭크 증가
+	p[v] = u;  // v 를 u의 자식으로 만든다.
+	return true;
+}
+
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> n;
+	cin >> n; 
+
+	priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> minheap;
 
 	for (int i = 0; i < n; i++)
 	{
@@ -23,24 +48,13 @@ int main() {
 		{
 			int cost;
 			cin >> cost;
+			if (i <= j) continue;
 
-			if (i == j) continue;
-
-			adj[i].push_back({cost, j});
-			adj[j].push_back({cost, i});
+			minheap.push({ cost,i,j });
 		}
 	}
-
-	int cnt = 0;
-
-	priority_queue < tuple<int, int, int>, vector <tuple<int, int, int>> , greater<tuple<int, int, int>>> minheap;
-
-	for (auto iter : adj[0])
-		minheap.push({ iter.first, 0, iter.second });
-
-	check[0] = 1;
 	
-
+	int cnt = 0;
 	long long ans = 0;
 
 	while (cnt < n - 1)
@@ -49,19 +63,16 @@ int main() {
 		tie(cost, a, b) = minheap.top();
 		minheap.pop();
 
-		if (check[b]) continue;
-		check[b] = 1;
-		ans += cost;
-
-		for (auto iter : adj[b])
-			if(!check[iter.second])
-				minheap.push({ iter.first,b,iter.second });
-
-		cnt++;
+		if (find(a) == find(b)) continue;
+		else
+		{
+			uni(a, b);
+			ans += cost;
+			cnt++;
+		}
 	}
 
 	cout << ans;
 
 	return 0;
-
 }
