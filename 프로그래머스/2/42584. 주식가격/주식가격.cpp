90c42fe757a22f6ebf90a7558c0b7bd4b959db9f
Tae Gwan Cha <38041722+chataeg@users.noreply.github.com>
2025-12-06 21:12:59 +0900

    [level 2] Title: 주식가격, Time: 26.26 ms, Memory: 24.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -5,32 +5,36 @@ using namespace std;
 
 vector<int> solution(vector<int> prices) {
     int n = prices.size(); 
+    
+    vector<int> ans(n,0);
     stack<int> s;
     
-    vector<int> answers(n,0);
+    // 1 3 5 4 3 1
+    // 4 3 1 1 1 0 
+    
+    // 1 2 3 2 2
+    // 4 3 1 1 0
+    
     
     for(int i = 0 ; i < n ; i++)
     {
         
-        
         while(!s.empty() && prices[s.top()] > prices[i])
         {
-            answers[s.top()] = i - s.top();
+            ans[s.top()] = i - s.top();
             s.pop();
         }
         
         s.push(i);
     }
-    
+
     while(!s.empty())
     {
         
-        answers[s.top()] = n - 1 - s.top();
-        
+        ans[s.top()] = n - 1 - s.top();
         s.pop();
     }
     
-    return answers;
-    
+    return ans;
     
 }
