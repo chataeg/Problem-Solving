d404d6e4c49b6f417b3bc34fc95d5e016d63756c
Tae Gwan Cha <38041722+chataeg@users.noreply.github.com>
2025-12-21 12:05:21 +0900

    [level 2] Title: 모음 사전, Time: 0.13 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -5,35 +5,41 @@
 
 using namespace std;
 
-int arr[5];
-int cnt = 0;
-string itoc = "AEIOU";
 string target;
+string itoc = "AEIOU";
 int ans;
+int cnt;
 
 void solve(string cur)
-{
+{   
+
     if(cur == target)
-    {  
+    {
         ans = cnt;
         return;
     }
     
     if(cur.length() >= 5)
-        return;
-    
-    for(int i = 0 ; i < 5 ; i++)
     {
-        if(ans != 0) return;
-        cnt++;
-        solve(cur + itoc[i]);    
+        return;
     }
     
+ 
     
+    for(int i = 0 ; i < 5 ; i++)
+    { 
+        if(ans != 0) return;
+        
+        cnt++;
+        solve(cur + itoc[i]);
+    }
 }
 
+
 int solution(string word) {
+ 
     target = word;
-    solve("");   
-    return cnt;
+    solve("");
+    
+    return ans;
 }
