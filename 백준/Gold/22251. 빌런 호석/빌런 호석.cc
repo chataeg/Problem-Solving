bb272d5cbc2b88b2c85377d19b44a909e91d5006
Tae Gwan Cha <38041722+chataeg@users.noreply.github.com>
2026-02-09 21:47:32 +0900

    [Gold V] Title: 빌런 호석, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/22251. 빌런 호석/빌런 호석.cc
+++ 백준/Gold/22251. 빌런 호석/빌런 호석.cc
@@ -12,22 +12,22 @@
 using namespace std;
 
 int n, k, p, x;
-string curfloor; 
+vector<int> curfloor;
 
 int change[10][10];
 
-string arr[10] =
+int arr[10] =
 {
-    "1110111",
-    "0010010",
-    "1011101",
-    "1011011",
-    "0111010",
-    "1101011",
-    "1101111",
-    "1010010",
-    "1111111",
-    "1111011"
+    0b1110111,
+    0b0010010,
+    0b1011101,
+    0b1011011,
+    0b0111010,
+    0b1101011,
+    0b1101111,
+    0b1010010,
+    0b1111111,
+    0b1111011
 };
 
 void inversion()
@@ -36,35 +36,27 @@ void inversion()
     {
         for (int j = 0; j < 10; j++)
         {
-            int cnt = 0;
-            for (int k = 0; k < 7; k++)
-                if (arr[j][k] ^ arr[i][k]) cnt++;
-
-            change[i][j] = cnt;
+            change[i][j] = __builtin_popcount(arr[j] ^ arr[i]);
         }
     }
 }
 
 int result;
 
-void solve(int depth, string cur, int cnt)
+void solve(int depth, int cur, int cnt)
 {
     if (cnt > p) return;
 
     if (depth == k)
     {
-        if (stoi(cur) >= 1 && stoi(cur) <= n)
+        if (cur >= 1 && cur <= n)
             result++;
         return;
     }
 
     for (int i = 0; i < 10; i++)
     {
-        cur.push_back(i + '0');
-        cnt += change[curfloor[depth]-'0'][i];
-        solve(depth+1,cur,cnt);
-        cur.pop_back();
-        cnt -= change[curfloor[depth]-'0'][i];
+        solve(depth+1,cur * 10 + i, cnt + change[curfloor[depth]][i]);
     }
 
 }
@@ -73,22 +65,17 @@ int main()
 {
     cin >> n >> k >> p >> x;
 
-    while (x > 0)
+    for(int i = 0 ; i < k ; i++)
     {
-        curfloor.push_back((x % 10) + '0');
+        curfloor.push_back((x % 10));
         x /= 10;
     }
-    int sze = curfloor.size();
-
-    for (int i = 0; i < k - sze; i++)
-        curfloor.push_back('0');
-
     reverse(curfloor.begin(), curfloor.end());
-
+    
     inversion();
 
-    solve(0,"",0);
+    solve(0,0,0);
 
-    cout << result-1;
+    cout << result - 1;
     return 0;
 }
