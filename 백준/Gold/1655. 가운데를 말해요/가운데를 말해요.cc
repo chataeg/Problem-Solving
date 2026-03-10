6e84481c0fecb681cb16c2fa5dc6e8ab50c2efcf
Tae Gwan Cha <38041722+chataeg@users.noreply.github.com>
2026-03-10 21:58:34 +0900

    [Gold II] Title: 가운데를 말해요, Time: 28 ms, Memory: 2792 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
@@ -1,44 +1,107 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+#include <string>
 #include <vector>
+#include <stack>
 #include <queue>
-#include <climits>
+#include <tuple>
+#include <deque>
+#include <map>
+#include <set>
 #include <algorithm>
+#include <bitset>
+#include <iostream>
 
 using namespace std;
 
-int n;
-
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	cin >> n;
-
-	priority_queue<int, vector<int>, greater<int>> minheap;
-	priority_queue<int> maxheap;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
 
-	while (n--)
-	{
-		int input;
-		cin >> input;
+    /*
+        1                       1
+        1 5                     1
+        1 2 5                   2
+        1 2 5 10                2
+      -99 1 2 5 10              2
+      -99 1 2 5 7  10           2
+      -99 1 2 5 5 7 10          5
+    */
 
-		if (maxheap.size() <= minheap.size())
-			maxheap.push(input);
-		else
-			minheap.push(input);
+    int n;
 
-		if (!minheap.empty() && maxheap.top() > minheap.top())
-		{
-			minheap.push(maxheap.top());
-			maxheap.pop();
-			maxheap.push(minheap.top());
-			minheap.pop();
-		}
+    cin >> n;
 
-		cout << maxheap.top() << '\n';
+    priority_queue<int,vector<int>,greater<int>> minheap;
+    priority_queue<int> maxheap;
 
-	}
-	return 0;
+
+    /*
+    
+        maxheap   minheap
+           
+           4        5
+           
+            
+    
+    */
+
+    for(int i = 0 ; i < n ; i++)
+    {
+        int input;
+        cin >> input;
+
+        if (i == 0)
+        {
+            maxheap.push(input);
+        }
+        else if (minheap.empty())
+        {
+            minheap.push(input);
+
+            if (maxheap.top() > minheap.top())
+            {
+                maxheap.push(minheap.top());
+                minheap.pop();
+
+                minheap.push(maxheap.top());
+                maxheap.pop();
+            }
+        }
+        else 
+        {
+            maxheap.push(input);
+
+            if (maxheap.top() > minheap.top())
+            {
+                maxheap.push(minheap.top());
+                minheap.pop();
+
+                minheap.push(maxheap.top());
+                maxheap.pop();
+            }
+        }
+
+        if ((i + 1) % 2 == 0)
+        {
+            if (maxheap.size() - minheap.size() >= 2)
+            {
+                minheap.push(maxheap.top());
+                maxheap.pop();
+
+            }
+
+            cout << (maxheap.top() < minheap.top() ? maxheap.top() : minheap.top()) << '\n';
+        }
+        else 
+        {
+            if (maxheap.size() < minheap.size())
+                cout << minheap.top() << '\n';
+            else
+                cout << maxheap.top() << '\n';
+        }
+
+    }
+
+    return 0;
 }
