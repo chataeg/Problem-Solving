6550ecd4f45e804206c16e9f2bd43c3cd9f325a0
Tae Gwan Cha <38041722+chataeg@users.noreply.github.com>
2026-03-31 20:09:19 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << "\\    /\\\n )  ( \')\n(  /  )\n \\(__)|";
+		
+	
+	return 0;
+}
